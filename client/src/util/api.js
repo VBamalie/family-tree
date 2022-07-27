804888d78903bc69328cc7bb148ba8ee6e49268f
Amalie Von Behren <vbamalie@gmail.com>
2022-07-27 17:15:54 -0400

    api front end

---
+++ client/src/util/api.js
@@ -0,0 +1,32 @@
+export const getVbNuclear = (nuclearId) => {
+    return fetch(`/api/vb/nuclear/${nuclearId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+export const getVbTree = (treeId) => {
+    return fetch(`/api/vb/nuclear/${treeId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+  export const getMicNuclear = (nuclearId) => {
+    return fetch(`/api/mic/nuclear/${nuclearId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
+export const getMicTree = (treeId) => {
+    return fetch(`/api/mic/nuclear/${treeId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
