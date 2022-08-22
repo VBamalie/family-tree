163782c790f054302608404ca5ef070619bc7b64
molly <vbamalie@gmail.com>
2022-08-22 16:56:27 -0400

    blah

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 // import { useNavigate } from "react-router-dom"
 
-import "./home.css"
+import "./homepage.css"
 import Image from 'react-bootstrap/Image'
 import Card from 'react-bootstrap/Card'
 
